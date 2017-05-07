9c81166043724a089e4021d1bd0f22a94d64faa5
Gylstorff, Quirin <Quirin.Gylstorff@gmx.net>
2017-05-07 07:08:41 -0700

    checkout emacs with own config

---
+++ configs/emacs.d/elpa/tiny-menu-20161213.435/tiny-menu-autoloads.el
@@ -0,0 +1,15 @@
+;;; tiny-menu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("tiny-menu.el") (22798 16546 939111 812000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tiny-menu-autoloads.el ends here
