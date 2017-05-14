ed46a5786fcbab24a4913679ebdfa2f4b8cf88ea
Gylstorff, Quirin <Quirin.Gylstorff@gmx.net>
2017-05-14 06:44:52 -0700

    move custom element to own custom.el
    to avoid cluttering of spacemacs.d/init.el
    move the custom variables,... to spacemacs.d/custom.el

--- configs/spacemacs.d/init.el
+++ configs/spacemacs.d/init.el
@@ -149,7 +149,7 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Hack"
-                               :size 22
+                               ;;:size 22
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
@@ -347,25 +347,12 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   ;; fci line delimiter
-  (set-fill-column 110)
+  (global-hl-line-mode -1)
+  (setq-default fill-column 110)
   (setq fci-rule-color "red")
   (setq fci-rule-width 3)
+  (turn-on-fci-mode)
   (spacemacs/add-to-hooks 'turn-on-fci-mode '(text-mode-hook))
   )
-
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (smeargle orgit magit-gitflow helm-gitignore gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit magit-popup which-key use-package pcre2el macrostep hydra help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx flx helm-descbinds helm-ag exec-path-from-shell evil-visualstar evil-escape evil goto-chg undo-tree elisp-slime-nav diminish bind-map bind-key auto-compile packed dash ace-window ace-jump-helm-line helm avy helm-core popup async))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+(load custom-file 'no-error 'no-message)
