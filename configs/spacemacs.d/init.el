0ebc92a450e7932dd2c65728064ba1772d833771
Gylstorff, Quirin <Quirin.Gylstorff@gmx.net>
2017-05-20 23:33:39 +0200

    add golang

--- configs/spacemacs.d/init.el
+++ configs/spacemacs.d/init.el
@@ -67,6 +67,7 @@ values."
      markdown
      org
      ess ;; R support
+     go
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
