2e38aeca05cf01a89a7649255935a93f048dadf4
Gylstorff, Quirin <Quirin.Gylstorff@gmx.net>
2017-05-21 22:51:23 +0200

    add python

--- configs/spacemacs.d/init.el
+++ configs/spacemacs.d/init.el
@@ -70,6 +70,7 @@ values."
           org-projectile-file "~/Dropbox/notes/TODOs.org")
      ess ;; R support
      go
+     python
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
