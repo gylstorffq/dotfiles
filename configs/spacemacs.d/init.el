01b2a0a7967844be6c38ce1c20e8ac97f6e9c3b6
Quirin Gylstorff <Quirin.Gylstorff@gmx.net>
2018-12-27 14:15:03 +0100

    Add latex layer to config

--- configs/spacemacs.d/init.el
+++ configs/spacemacs.d/init.el
@@ -80,6 +80,7 @@ values."
      go
      plantuml
      python
+     latex
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
